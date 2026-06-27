1356020e89b13c083015c8fad98b82478fe47b0b
abhishek-butter <abhishekvaiybhav@gmail.com>
2026-06-27 13:38:55 +0530

    Implemented Interactive CLI Expense Tracker

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,3 +1,4 @@
+
 import java.time.*;
 import java.util.ArrayList;
 
@@ -21,20 +22,26 @@ class ExpenseTracker{
     public void addExpense(int amount,LocalDate date,String category){
         
         Expense a=new Expense(nextId,amount,date,category);
-        System.out.println("Your expense id is "+nextId);
+        System.out.println("\nYour expense id is "+nextId);
+        
         expenses.add(a);
         nextId++;
         
         
 
     }
-    public void deleteExpense(int id){
+    public boolean deleteExpense(int id){
+        boolean found=false;
+        
         for(int i=0;i<expenses.size();i++){
             if(expenses.get(i).getId()==id){
                 expenses.remove(i);
+                found=true;
+
                 break;
             }
         }
+        return found;
     }
     public void viewExpenses(){
         if(expenses.isEmpty()){
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,75 @@
-package src;
+
+import java.util.*;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String args[]){
-        
+        System.out.println("Welcome to Your Personal Expense Tracker");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name=sc.nextLine();
+        ExpenseTracker tracker=new ExpenseTracker(name);
+        while (true) {
+            
+            
+            System.out.print("\n===========================================================\nExpense Tracker Menu\n===========================================================\n");
+            System.out.print("\n1.Add Expense\n2.Delete Expense\n3.View Expenses\n4.SearchExpense\n5.EXIT\n");
+            System.out.print("Please enter you choice here: ");
+            
+            int n=sc.nextInt();
+            sc.nextLine();
+            
+            
+            switch(n){
+                case 1 ->{
+                    System.out.print("\nEnter Amount: ");
+                    int amount=sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter day (DD): ");
+                    int day=sc.nextInt();
+                    System.out.print("Enter month (MM): ");
+                    int month=sc.nextInt();
+                    System.out.print("Enter year (YYYY): ");
+                    int year=sc.nextInt();
+                    sc.nextLine();
+                    LocalDate l=LocalDate.of(year,month,day);
+                    System.out.print("Enter category: ");
+                    String category=sc.nextLine();
+                    tracker.addExpense(amount, l, category);
+    
+                }
+
+                case 2->{
+                    System.out.print("Enter id of the expense you want to delete: ");
+                    int deleteid=sc.nextInt();
+                    sc.nextLine();
+                    boolean check=tracker.deleteExpense(deleteid);
+                    if(check)System.out.println("Deleted Succesfully");
+                    else System.out.println("Expense not found ");
+
+                    
+                }
+                case 3->{
+                    tracker.viewExpenses();
+                    
+                }
+                case 4->{
+                    System.out.print("Enter id of the expense you want to search: ");
+                    int searchid=sc.nextInt();
+                    sc.nextLine();
+                    tracker.searchExpense(searchid);
+                    
+                }
+                case 5-> {
+                    return;
+                }
+                default ->{
+                    System.out.println("enter a valid choice");
+                }
+            }
+            
+        }
+   
     }
     
 }
